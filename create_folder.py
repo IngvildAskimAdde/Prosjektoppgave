483bc50d975ccebbc8512b421ecaecceb396ec02
Ingvild Askim Adde <ingvild@dhcp-10-22-7-73.wlan.ntnu.no>
2020-09-23 15:13:04 +0200

    Loading folder file

---
+++ create_folder.py
@@ -0,0 +1,12 @@
+# %%
+import os
+
+src_main = r'D:\LARC_T2' #Change to your path
+dst_main = r'D:\LARC_T2_cleaned' #Change to your path
+patient_identifier = 'LARC-RRP'
+scan_time_point_folder = 'MRS1'
+
+patient_list = [i for i in os.listdir(src_main_folder) if i.startswith(patient_identifier)]
+
+for patient in patient_list:
+    os.makedirs( os.path.join(dst_main_folder, patient, scan_time_point_folder) )
