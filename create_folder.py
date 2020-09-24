582b3997d68464f2749f1ec0b653ad2a51dff3d7
Ingvild Askim Adde <ingvild@dhcp-10-22-7-73.wlan.ntnu.no>
2020-09-24 12:00:16 +0200

    Changed path name

--- create_folder.py
+++ create_folder.py
@@ -1,8 +1,8 @@
 # %%
 import os
 
-src_main_folder = r'D:\LARC_T2' #Change to your path
-dst_main_folder = r'D:\LARC_T2_cleaned' #Change to your path
+src_main_folder = r'/Volumes/Untitled/LARC_T2_cleaned' #Change to your path
+dst_main_folder = r'/Volumes/Untitled/LARC_T2_cleaned_nii' #Change to your path
 patient_identifier = 'LARC-RRP'
 scan_time_point_folder = 'MRS1'
 
