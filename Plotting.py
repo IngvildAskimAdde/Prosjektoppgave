9f4b78951351aa0e18828384426107dc732f25cb
IngvildAskimAdde <ingvilad@stud.ntnu.no>
2020-11-03 09:40:44 +0100

    Adding option for viewing png files

--- Plotting.py
+++ Plotting.py
@@ -2,12 +2,13 @@ import get_data
 from Preprocessing import ImageViewer as iv
 import SimpleITK as sitk
 import numpy as np
+import matplotlib.pyplot as plt
 
 main_folder = r'/Volumes/Untitled/LARC_T2_preprocessed'
 image_prefix = 'image'
 mask_suffix = 'label.nii'
 result_suffix='.nii'
-
+"""
 patientsPaths, patientsNames, patientsPaths_image, patientsPaths_groundTruth = get_data.get_paths(main_folder, image_prefix, mask_suffix)
 
 
@@ -16,6 +17,7 @@ mask_array, masksize = get_data.get_array(patientsPaths_groundTruth[89])
 result_array, resultsize = get_data.get_array('/Volumes/Untitled/Results/ID_0/LARC-RRP-001.nii')
 org_array, org_size = get_data.get_array('/Volumes/Untitled/LARC_T2_cleaned_nii/LARC-RRP-001/MRS1/image.nii')
 orgmask_array, orgmask_size = get_data.get_array('/Volumes/Untitled/LARC_T2_cleaned_nii/LARC-RRP-001/MRS1/1 RTSTRUCT LARC_MRS1-label.nii')
+"""
 
 #Creates an image object from a 1D array
 def create_image_from_array(array, imsize):
@@ -23,12 +25,13 @@ def create_image_from_array(array, imsize):
     im = im.astype(int)
     im = sitk.GetImageFromArray(im)
     return im
-
+"""
 im = create_image_from_array(img_array,imsize)
 mask = create_image_from_array(mask_array,masksize)
 result = create_image_from_array(result_array,resultsize)
 org = create_image_from_array(org_array, org_size)
 orgmask = create_image_from_array(orgmask_array, orgmask_size)
+"""
 
 def show_image(image, mask, image_prefix, mask_suffix):
     v = iv.Viewer(view_mode='1', mask_to_show=['a'])
@@ -36,8 +39,18 @@ def show_image(image, mask, image_prefix, mask_suffix):
     v.set_mask(mask, label=mask_suffix)
     v.show()
 
+def view_png(path):
+    image = plt.imread(path)
+    plt.figure()
+    plt.imshow(image)
+    plt.axis('off')
+
+view_png('/Users/ingvild/Desktop/011_cropped_nifty.png')
+
+"""
 #Displays the image and mask in figure
 v = iv.Viewer(view_mode='1', mask_to_show=['a'])
 v.set_image(im, label=image_prefix)
 v.set_mask(mask, label=mask_suffix)
 v.show()
+"""
