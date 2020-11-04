dd485754382473870e1ea1c2b2f5802b207f8e5a
IngvildAskimAdde <ingvilad@stud.ntnu.no>
2020-11-04 14:09:15 +0100

    Creating folders for postprocessed masks

--- create_folder.py
+++ create_folder.py
@@ -1,12 +1,13 @@
 # %%
 import os
 
-src_main_folder = r'/Volumes/Untitled/LARC_T2_cleaned' #Change to your path
-dst_main_folder = r'/Volumes/Untitled/LARC_T2_cleaned_nii' #Change to your path
-patient_identifier = 'LARC-RRP'
-scan_time_point_folder = 'MRS1'
+src_main_folder = r'/Volumes/Untitled/Results' #Change to your path
+dst_main_folder = r'/Volumes/Untitled/Results_postprocessed' #Change to your path
+#patient_identifier = 'LARC-RRP'
+patient_identifier = 'ID'
+#scan_time_point_folder = 'MRS1'
 
 patient_list = [i for i in os.listdir(src_main_folder) if i.startswith(patient_identifier)]
 
 for patient in patient_list:
-    os.makedirs( os.path.join(dst_main_folder, patient, scan_time_point_folder) )
+    os.makedirs( os.path.join(dst_main_folder, patient))#, scan_time_point_folder) )
